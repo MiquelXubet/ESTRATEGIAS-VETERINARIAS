0c38a78d7971685f7499607b8a2dfc451b3338a0
Miki <miquelxubet@gmail.com>
2025-06-12 15:53:13 +0200

    sección "cómo podemos ayudarte"

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Topheader from "./Components/Top-header/Header";
 import Mainheader from "./Components/Main-Header/Main-header";
 import Hero from "./Components/Banner/Hero";
 import Servicios from "./Components/Servicios/Servicios";
+import ComoAyudamos from "./Components/Ayuda/ComoAyudamos";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Topheader />
       <Mainheader />
       <Hero />
+      <ComoAyudamos />
       <Servicios />
     </div>
   );
---
+++ src/Components/Ayuda/ComoAyudamos.js
@@ -0,0 +1,23 @@
+import ayuda from "../../Data/AyudaData";
+import "./ComoAyudamos.css";
+import React from "react";
+
+function ComoAyudamos() {
+  return (
+    <section className="como-ayudamos">
+      <h2>Cómo podemos ayudarte</h2>
+      {ayuda.map((item) => (
+        <div className="punto-fuerte" key={item.titulo}>
+          <div className="primera-linea">
+            <div className="icono">{item.icon}</div>
+            <h3 className="titulo">{item.titulo}</h3>
+          </div>
+          <p className="descripcion">{item.descripcion}</p>
+          <p className="extra">{item.extra}</p>
+        </div>
+      ))}
+    </section>
+  );
+}
+
+export default ComoAyudamos;
