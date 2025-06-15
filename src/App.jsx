727209cf0b21fe532d0d5f5d823342b78e9eb873
Miki <miquelxubet@gmail.com>
2025-06-15 19:55:06 +0200

    incorporación router layout y pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
 import "./App.css";
-import Topheader from "./Components/Top-header/Header";
-import Mainheader from "./Components/Main-Header/Main-header";
-import Hero from "./Components/Banner/Hero";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./Components/Layout/Layout";
+import Home from "./Components/Pages/Home";
+import Contacto from "./Components/Pages/Contacto";
 import Servicios from "./Components/Servicios/Servicios";
-import ComoAyudamos from "./Components/Ayuda/ComoAyudamos";
-import Testimonios from "./Components/Testimonios/Testimonios";
-import Formulario from "./Components/Formulario/Formulario";
-import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
-      <Topheader />
-      <Mainheader />
-      <Hero />
-      <ComoAyudamos />
-      <Servicios />
-      <Testimonios />
-      <Formulario />
-      <Footer />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="Contacto" element={<Contacto />} />
+            <Route path="Servicios" element={<Servicios />} />
+            {/* Add more routes here as needed */}
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Footer from "../components/Footer";
-import Header from "../components/Header";
-import Mainheader from "./Components/Main-Header/Main-header";
-import "./Layout.css";
+import Topheader from "../Top-header/Header";
+import Mainheader from "../Main-Header/Main-header";
+import Footer from "../Footer/Footer";
 
 function Layout() {
   return (
     <>
-      <Header />
+      <Topheader />
       <Mainheader />
       <main>
         <Outlet />
